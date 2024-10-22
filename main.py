9bf7703dc570be7433be193214bf2ab058d97da3
Dylan Cutshaw <odobenuspotato@gmail.com>
2024-10-22 13:29:24 -0400

    B: added player movement

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pygame
 from constants import *
- 
+from circleshape import * 
+from player import * 
 def main(): 
 	pygame.init() 	
+	clock = pygame.time.Clock()
+	dt = 0 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	x = SCREEN_WIDTH / 2
+	y = SCREEN_HEIGHT / 2 
+	player = Player(x, y) 
+
 	while True: 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: 
+				pygame.quit() 
 				return 
-	screen.fill((0,0,0)) 
-	pygame.display.flip() 
+		screen.fill((0,0,0)) 
+		player.update(dt)
+		player.draw(screen)
+		pygame.display.flip()
+			
+		dt = clock.tick(60) / 1000  
 if __name__ == "__main__": 
 	main() 
 
